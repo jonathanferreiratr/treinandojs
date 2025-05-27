44f4a9dce5a748f3423da81c8db20f7b580a6d2b
jonathanferreiratr <jonathanferreiratr@gmail.com>
2025-05-26 21:01:07 -0300

    Corrige estrutura de repetição em EstruturaDeRepeticao.js

--- EstrturaDeRepeticao.js
+++ EstrturaDeRepeticao.js
@@ -4,7 +4,7 @@
 // DO ...while => faça ... enquanto
 
 
-var i = 0; // inicializa a variavel i com 0
+/*var i = 0; // inicializa a variavel i com 0
 
 do{
     console.log(i);
@@ -13,3 +13,26 @@ do{
 
 // o laço do...while garante que o bloco de código seja executado
 //  pelo menos uma vez, mesmo que a condição seja falsa na primeira iteração. 
+
+
+
+// contagem de dois em dois etc.
+var i = 0;
+do{
+    console.log("do",i);
+    i++; //incrementa i de 2 em 2
+}while(i < 10);
+// o laço do...while garante que o bloco de código seja executado
+ 
+i = 0; // reinicializa a variavel i com 0
+
+while(i < 10){
+    console.log("while", i);
+    i++; // incrementa i de 1 em 1
+}
+*/
+
+lista_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // cria uma lista de numeros de 1 a 10 array
+for ( i in lista_numeros){
+    console.log("for", i, lista_numeros[i]); // imprime o indice e o valor do elemento da lista
+}
